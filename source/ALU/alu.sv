7e554d643d3e23e8222faa528ccaeb5b555bb180
Elijah Jorgensen <jorgenel@purdue.edu>
2023-06-15 09:20:02 -0400

    ALU V0.88

--- source/ALU/alu.sv
+++ source/ALU/alu.sv
@@ -32,19 +32,27 @@ always_comb begin : ALU_6502
     V = 0; // Overflow false
 
     case(OP)
-        ADD:begin y = A + B ; 
-        SUB: y = A - B;
+        ADD:begin 
+            y = A + B ; 
+            C = Y < A | Y < B;
+            V = (A[7] == B[7]) && (A[7] != y[7]); // 1 if MSB of A and B are equal and MSB of Y is different
+        end
+        SUB: begin
+            y = A - B;
+            C = A < B;
+            V = (A[7] != B[7]) && (A[7] != y[7]); // 1 if MSB of A and B are different and MSB of Y is different
+        end
         AND: y = A & B;
         OR:  y = A | B;
         XOR: y = A ^ B;
-        SHL: y = A << 1;
-        SHR: y = A >> 1;
+        SHL: {C,Y} = {1'b0,A} << 1;
+        SHR: {Y,C} = {A,1'b0} >> 1;
         default: y = 0;
         endcase
     
     // Set flags
-    Z = ~(|y);  // 1 if all bits are 0
-    N = y[7];   // 1 if MSB is 1
+    Z = ~(|Y);  // 1 if all bits are 0
+    N = Y[7];   // 1 if MSB is 1
     V = (A[7] == B[7]) && (A[7] != y[7]); // 1 if MSB of A and B are equal and MSB of Y is different
     Y = y;      // Output
 end
